bca377f4129e2d2cbc02bac3fadaf7f1306926c9
rakeshboyiri <boyirirakesh@gmail.com>
2025-03-19 02:43:36 +0530

    files-changed

--- src/components/logo-container.tsx
+++ src/components/logo-container.tsx
@@ -4,9 +4,9 @@ export const LogoContainer = () => {
   return (
     <Link to={"/"}>
       <img
-        src="/assets/svg/logo.svg"
+        src="public/assets/img/logo/rguktlogo.png"
         alt=""
-        className="min-w-10 min-h-10 object-contain"
+        className="max-w-12 min-h-10 object-contain"
       />
     </Link>
   );
--- src/routes/home.tsx
+++ src/routes/home.tsx
@@ -1,10 +1,10 @@
-import { Sparkles } from "lucide-react";
+// import { Sparkles } from "lucide-react";
 import Marquee from "react-fast-marquee";
 
 import { Container } from "@/components/container";
-import { Button } from "@/components/ui/button";
+// import { Button } from "@/components/ui/button";
 import { MarqueImg } from "@/components/marquee-img";
-import { Link } from "react-router-dom";
+// import { Link } from "react-router-dom";
 
 const HomePage = () => {
   return (
@@ -13,7 +13,7 @@ const HomePage = () => {
         <div className="my-8">
           <h2 className="text-3xl text-center md:text-left md:text-6xl">
             <span className=" text-outline font-extrabold md:text-8xl">
-              AI Superpower
+              AI Virtual Interview System
             </span>
             <span className="text-gray-500 font-extrabold">
               - A better way to
@@ -29,23 +29,10 @@ const HomePage = () => {
           </p>
         </div>
 
-        <div className="flex w-full items-center justify-evenly md:px-12 md:py-16 md:items-center md:justify-end gap-12">
-          <p className="text-3xl font-semibold text-gray-900 text-center">
-            250k+
-            <span className="block text-xl text-muted-foreground font-normal">
-              Offers Recieved
-            </span>
-          </p>
-          <p className="text-3xl font-semibold text-gray-900 text-center">
-            1.2M+
-            <span className="block text-xl text-muted-foreground font-normal">
-              Interview Aced
-            </span>
-          </p>
-        </div>
+      
 
         {/* image section */}
-        <div className="w-full mt-4 rounded-xl bg-gray-100 h-[420px] drop-shadow-md overflow-hidden relative">
+        {/* <div className="w-full mt-4 rounded-xl bg-gray-100 h-[420px] drop-shadow-md overflow-hidden relative">
           <img
             src="/assets/img/hero.jpg"
             alt=""
@@ -57,17 +44,17 @@ const HomePage = () => {
           </div>
 
           <div className="hidden md:block absolute w-80 bottom-4 right-4 px-4 py-2 rounded-md bg-white/60 backdrop-blur-md">
-            <h2 className="text-neutral-800 font-semibold">Developer</h2>
-            <p className="text-sm text-neutral-500">
+            <h2 className="text-neutral-800 font-semibold">Developer</h2> */}
+            {/* <p className="text-sm text-neutral-500">
               Lorem ipsum dolor sit amet consectetur adipisicing elit. Numquam
               distinctio natus, quos voluptatibus magni sapiente.
-            </p>
+            </p> */}
 
-            <Button className="mt-3">
+            {/* <Button className="mt-3">
               Generate <Sparkles />
-            </Button>
-          </div>
-        </div>
+            </Button> */}
+          {/* </div>
+        </div> */}
       </Container>
 
       {/* marquee section */}
@@ -84,7 +71,7 @@ const HomePage = () => {
         </Marquee>
       </div>
 
-      <Container className="py-8 space-y-8">
+      {/* <Container className="py-8 space-y-8">
         <h2 className="tracking-wide text-xl text-gray-800 font-semibold">
           Unleash your potential with personalized AI insights and targeted
           interview practice.
@@ -113,7 +100,7 @@ const HomePage = () => {
             </Link>
           </div>
         </div>
-      </Container>
+      </Container> */}
     </div>
   );
 };
